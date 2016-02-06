7a86f130cf377945f9a097e38d1485150b029375
Edward Askew <edward.askew@foonet.me.uk>
2016-02-06 22:31:26 +0000

    Modified xfcp.py to use new lib package, fixed bugs

---
+++ lib/__init__.py
@@ -0,0 +1,3 @@
+from .character import Character
+from .properties import PropertyType, IntProperty
+from .pool import CharacterPool
--- lib/properties.py
+++ lib/properties.py
@@ -12,33 +12,37 @@ class PropertyType(type):
     def __new__(cls, name, bases=None, namespace=None, **kwargs):
 
         # Allows Proprty Types to be retrived by string name as ProprtyType(str)
-        if name in KnownTypes:
-            return KnownTypes[name]
+        if name in cls.KnownTypes:
+            return cls.KnownTypes[name]
 
         #if we called PropertyType with just a string and didn't find it in
         #KnownTypes we need to throw an error
         if bases is None or namespace is None:
             raise TypeError("Unknown PropertyType: {}".format(name))
 
-        result = type.__new__(cls, name, namespace)
+        result = type(name, bases, namespace)
 
         #lets us define abstract base classes that don't go into the KnowTypes
         #dict by leaving off the typename property
         if hasattr(result, 'typename'):
-            KnownTypes[result.typename] = result
+            cls.KnownTypes[result.typename] = result
 
         return result
 
 class Property(metaclass=PropertyType):
-    """Base class to give all properties the PropertyType metaclass"""
+    """Base class for properties"""
 
     def __init__(self, name, value=None):
         self.name = name
         if value is not None:
             self.value = value
 
+    def __str__(self):
+        return str(self.value)
+
     def unpack(self, data):
         self.value = self._unpack(data)
+        return self
 
     @classmethod
     def _unpack(cls, data):
@@ -51,7 +55,7 @@ class Property(metaclass=PropertyType):
         format"""
         return cfile.read(size)
 
-class IntProperty(Property):
+class IntProperty(Property, metaclass=PropertyType):
     """Integer Property - represented as a little endian DWORD"""
 
     typename = 'IntProperty'
@@ -60,13 +64,13 @@ class IntProperty(Property):
     def _unpack(cls, data):
         return struct.unpack('<i', data)[0]
 
-class ArrayProperty(IntProperty):
+class ArrayProperty(IntProperty, metaclass=PropertyType):
     """Array Property - acts as an IntProperty with value of the number of
     elements in the array"""
 
     typename = 'ArrayProperty'
 
-class BoolProperty(Property):
+class BoolProperty(Property, metaclass=PropertyType):
     """Boolean Property - represented by a single byte, 0x00 is False anything
     else is True"""
 
@@ -83,7 +87,7 @@ class BoolProperty(Property):
             size = 1
         return super().read_data(cfile, size)
 
-class StrProperty(Property):
+class StrProperty(Property, metaclass=PropertyType):
     """String Property - repersented by a little endian DWORD containing the
     string length followed by a null terminated string - assuming latin-1
     encoding"""
@@ -91,13 +95,13 @@ class StrProperty(Property):
     typename = 'StrProperty'
 
     @classmethod
-    def _unpack(data):
-        size = IntProperty.unpack_int(data[:4])
-        if len(bstr[4:]) != size:
+    def _unpack(cls, data):
+        size = IntProperty._unpack(data[:4])
+        if len(data[4:]) != size:
             raise PropertyError("Incorrect String Size in StrProperty: {}".format(size))
-        return bstr[4:-1].decode("latin_1")
+        return data[4:-1].decode("latin_1")
 
-class NameProperty(Property):
+class NameProperty(Property, metaclass=PropertyType):
     """Name Property - represented as a StrProperty followed by a DWORD that is
     usually (but not always) 0. As the function of this DWORD is unknown for
     the moment we just represent the value of NameProperty as a (str, int)
@@ -106,12 +110,12 @@ class NameProperty(Property):
     typename = 'NameProperty'
 
     @classmethod
-    def _unpack(self, data):
+    def _unpack(cls, data):
         name = StrProperty._unpack(data[:-4])
         val = IntProperty._unpack(data[-4:])
         return (name, val)
 
-class StructProprty(Property):
+class StructProprty(Property, metaclass=PropertyType):
     """Struct Property - a struct represented as a sequence of properties ended
     with 'None'"""
 
@@ -125,6 +129,10 @@ class StructProprty(Property):
             for prop in parser.properties():
                 setattr(self, prop.name, prop)
 
+        return self
+
+    def __str__(self):
+        return self.name
 
     @classmethod
     def read_data(cls, cfile, size):
